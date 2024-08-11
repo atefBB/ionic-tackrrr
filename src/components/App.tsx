0ada547f600d3bde4954ab6b6b7e02dc1f28109f
Atef Ben Ali <atef.bettaib@gmail.com>
2024-08-11 18:37:32 +0100

    build: add `ionic`

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,5 @@
 import { Fragment, useState, useEffect } from "react";
+import { IonApp, IonPage } from "@ionic/react";
 
 import supabase from "../lib/supabase";
 
@@ -6,6 +7,11 @@ import { AuthForm } from "./AuthForm";
 import { Home } from "../pages/Home";
 import Header from "../components/Header";
 import { Footer } from "./Footer";
+
+// eslint-disable-next-line
+import { setupIonicReact } from "@ionic/react";
+
+//import "@ionic/react/css/core.css";
 
 export function App() {
   const [user, setUser] = useState<any>(null);
@@ -28,18 +34,20 @@ export function App() {
   }, []);
 
   return (
-    <main>
-      {user === null ? (
-        <div className="container">
-          <AuthForm />
-        </div>
-      ) : (
-        <Fragment>
-          <Header user={user} />
-          <Home user={user} />
-        </Fragment>
-      )}
-      <Footer />
-    </main>
+    <IonApp>
+      <IonPage>
+        {user === null ? (
+          <div className="container">
+            <AuthForm />
+          </div>
+        ) : (
+          <Fragment>
+            <Header user={user} />
+            <Home user={user} />
+          </Fragment>
+        )}
+        <Footer />
+      </IonPage>
+    </IonApp>
   );
 }
