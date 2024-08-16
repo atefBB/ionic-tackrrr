d9e176fe44f8c6d1c44a7452527a07e3d8727491
Atef Ben Ali <atef.bettaib@gmail.com>
2024-08-16 23:17:54 +0100

    build: add config files

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.trackrr.app',
+  appName: 'Ionic Trackrr',
+  webDir: 'build'
+};
+
+export default config;
